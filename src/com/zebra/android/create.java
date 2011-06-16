e92bf6acb64b83c3ec37a32aa00e58592be40429
Matthew Erickson <ericksonm1029@net.elmhurst.edu>
2011-06-16 16:25:21 -0500

    menu system working now. YEAH!!!!
    Change-Id: I74a9f12bdae71f14c323c8c72a22e326ec550a13

--- src/com/zebra/android/create.java
+++ src/com/zebra/android/create.java
@@ -1,13 +1,24 @@
 package com.zebra.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class create extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.create_label);
+		
+		Button main = (Button) findViewById(R.id.cmain);
+		main.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				Intent intent = new Intent();
+        		setResult(RESULT_OK, intent);
+        		finish();
+			}
+		});
 	}
-	
 }
--- src/com/zebra/android/mainmenu.java
+++ src/com/zebra/android/mainmenu.java
@@ -3,6 +3,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class mainmenu extends Activity {
     /** Called when the activity is first created. */
@@ -10,14 +11,30 @@ public class mainmenu extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }    
     
-    public void gotocreate(View create){
-    	Intent i = new Intent();  
-		i.setClassName("com.zebra.android",                  
-				"com.zebra.android.create");         
-		startActivity(i);  
-
+        Button createbutton = (Button) findViewById(R.id.create);
+        createbutton.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View view){
+        		Intent i = new Intent(view.getContext(), create.class);  
+        		startActivityForResult(i, 0);  
+        	}
+        });
+        
+        Button reprintbutton = (Button) findViewById(R.id.reprint);
+        reprintbutton.setOnClickListener(new View.OnClickListener() {
+		  	public void onClick(View view){
+        		Intent i = new Intent(view.getContext(), reprint.class);  
+        		startActivityForResult(i, 0);  
+        	}
+        });
+        
+        Button inventorybutton = (Button) findViewById(R.id.inventory);
+        inventorybutton.setOnClickListener(new View.OnClickListener() {
+		  	public void onClick(View view){
+        		Intent i = new Intent(view.getContext(), inventory.class);  
+        		startActivityForResult(i, 0);  
+        	}
+        });
     }
 }
 
