c7cd0950d9165610467af21c31db6e5fb5d9db09
Matthew Erickson <ericksonm1029@net.elmhurst.edu>
2011-06-16 15:03:12 -0500

    added buttons
    Change-Id: I512c7884bb0019f0d74c56b4e1136af22d8ee815

--- src/com/zebra/android/mainmenu.java
+++ src/com/zebra/android/mainmenu.java
@@ -1,7 +1,9 @@
 package com.zebra.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class mainmenu extends Activity {
     /** Called when the activity is first created. */
@@ -9,6 +11,13 @@ public class mainmenu extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        /**hi*/
+    }    
+    
+    public void gotocreate(View create){
+    	Intent i = new Intent();  
+		i.setClassName("com.zebra.android",                  
+				"com.zebra.android.create");         
+		startActivity(i);  
     }
+    
 }
