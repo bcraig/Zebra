dcbdf979dd62398dea3594418f1db3aac98243c2
Benjamin Craig <bcraig@wisc.edu>
2011-06-17 14:53:08 -0500

    added influence from ZSDK demo, create_label
    Change-Id: Ic12245573176453a283163b9b48f59a7ddeabc07

--- src/com/zebra/android/CreateLabel.java
+++ src/com/zebra/android/CreateLabel.java
@@ -1,8 +1,12 @@
 package com.zebra.android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnKeyListener;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnFocusChangeListener;
 import android.widget.Button;
@@ -11,12 +15,13 @@ import android.widget.TextView;
 
 
 public class CreateLabel extends Activity {
+	private EditText serial;
 	protected void onCreate(Bundle savedInstanceState) {
-		String serialNumber;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.create_label);
 		
-		Button CtoMain = (Button) findViewById(R.id.c_to_main);
+		
+		final Button CtoMain = (Button) findViewById(R.id.c_to_main);
 		CtoMain.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
 				Intent intent = new Intent();
@@ -25,24 +30,34 @@ public class CreateLabel extends Activity {
 			}
 		});
 		
-		final EditText serial = (EditText) findViewById(R.id.manual_serial);
-		serial.setOnFocusChangeListener(new OnFocusChangeListener(){
-			
-			public void onFocusChange(View v, boolean hasFocus){
-				if(hasFocus==true){
-					serial.setText("");
-				}
-			}
-		});
-		
-		
-		
-		if(serial.getText().toString().length()>=7){
-			//have serial
-			serialNumber = serial.getText().toString();
-			TextView text = (TextView) findViewById(R.id.create_text1);
-			text.setText(serialNumber);
-		}
-		
+		serial = (EditText)findViewById(R.id.manual_serial);
+
 	}
+		
+	
+	public boolean onKey(View v, int keyCode, KeyEvent event){
+		if((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)){
+			//enter key pressed
+			String serialNumber = serial.getText().toString();
+			
+			
+			
+			return true;
+		}
+		return false;
+	}
+	
+	private void toggleEditField(EditText editText, boolean set) {
+		/*
+		 * Note: Disabled EditText fields may still get focus by some other means, and allow text input.
+		 *       See http://code.google.com/p/android/issues/detail?id=2771
+		 */
+		editText.setEnabled(set);
+		editText.setFocusable(set);
+		editText.setFocusableInTouchMode(set);
+	}
+    private String getSerialFieldText() {
+        return serial.getText().toString();
+    }
+	
 }
