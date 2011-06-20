003c6e49a56c020afd17e2643ad5cf9d9a8c4c60
Benjamin Craig <bcraig@wisc.edu>
2011-06-20 09:16:05 -0500

    scan loading into all screens
    Change-Id: I33a198afd205ed2830fe33ede57b853e206dbda6

--- src/com/zebra/android/Inventory.java
+++ src/com/zebra/android/Inventory.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class Inventory extends Activity {
+	private EditText serial;
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.inventory);
@@ -20,5 +23,36 @@ public class Inventory extends Activity {
         		finish();
 			}
 		});
+		
+		serial = (EditText) findViewById(R.id.inventory_manual_serial);
 	}
+	
+	public void startScanner(View v){
+		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+		intent.putExtra("com.google.zxing.client.android.SCAN.SCAN_MODE", 
+		"QR_CODE_MODE"); 
+		startActivityForResult(intent, 0);
+		
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {     
+		if (requestCode == 0) {         
+			if (resultCode == RESULT_OK) {             
+				String contents = intent.getStringExtra("SCAN_RESULT");             
+				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");   
+			
+				serial.setText(contents);
+				// Handle successful scan 
+
+				/*onCreateDialog(DIALOG_SCAN_COMPLETE, temp);
+				Bundle temp = new Bundle();
+				temp.putString(SCAN_CONTENTS_KEY, contents);*/
+
+
+				
+			} else if (resultCode == RESULT_CANCELED) {             
+				// Handle cancel         
+			}   
+		}
+	} 
 }
--- src/com/zebra/android/MainMenu.java
+++ src/com/zebra/android/MainMenu.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainMenu extends Activity {
+
     /** Called when the activity is first created. */
-    @Override
+    @Override    
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
