075e21045c96cdbf627e380ab3c8f47945a08c0f
Benjamin Craig <bcraig@wisc.edu>
2011-06-19 20:22:43 -0500

    SCANNER READING WORKS!
    Change-Id: Ibd49d28cfc7ef5bac3e364db8be6df1ed84432de

--- src/com/zebra/android/CreateLabel.java
+++ src/com/zebra/android/CreateLabel.java
@@ -2,6 +2,7 @@ package com.zebra.android;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnKeyListener;
 import android.content.Intent;
@@ -17,6 +18,8 @@ import android.widget.TextView;
 
 public class CreateLabel extends Activity {
 	private EditText serial;
+	private final int DIALOG_SCAN_COMPLETE = 1;
+	private final String SCAN_CONTENTS_KEY = "key";
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.create_label);
@@ -30,6 +33,8 @@ public class CreateLabel extends Activity {
         		finish();
 			}
 		});
+		
+		serial = (EditText) findViewById(R.id.manual_serial);
 	}	
 
 	/*	Button LaunchScan = (Button) findViewById(R.id.create_scan);
@@ -64,27 +69,59 @@ public class CreateLabel extends Activity {
 			text.setText(serialNumber);
 		}*/
 	
+	
+	public void startScanner(View v){
+		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+		intent.putExtra("com.google.zxing.client.android.SCAN.SCAN_MODE", 
+		"QR_CODE_MODE"); 
+		startActivityForResult(intent, 0);
+		
+	}
+	
 	/*code below from http://stackoverflow.com/questions/2050263/using-zxing-to-create-an-android-barcode-scanning-app */
-	public Button.OnClickListener mScan = new Button.OnClickListener() {
+	/*public Button.OnClickListener mScan = new Button.OnClickListener() {
 		public void onClick(View v) {
-			Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-			intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-			startActivityForResult(intent, 0); 
+			
 		}
-	} ;  
+	} ;  */
+
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {     
 		if (requestCode == 0) {         
 			if (resultCode == RESULT_OK) {             
 				String contents = intent.getStringExtra("SCAN_RESULT");             
-				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");             
-				// Handle successful scan         
+				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");   
+			
+				serial.setText(contents);
+				// Handle successful scan 
+
+				/*onCreateDialog(DIALOG_SCAN_COMPLETE, temp);
+				Bundle temp = new Bundle();
+				temp.putString(SCAN_CONTENTS_KEY, contents);*/
+
+
+				
 			} else if (resultCode == RESULT_CANCELED) {             
 				// Handle cancel         
 			}   
 		}
 	} 
-
+	
+	/*protected Dialog onCreateDialog(int Id, Bundle args){
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(getSerialFieldText()).setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				//MyActivity.this.finish();           
+				}
+			}).setNegativeButton("No", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {                
+				dialog.cancel();           
+				}       
+			});AlertDialog alert = builder.create();
+			return alert;
+	}
+	protected void onPrepareDialog(int Id, Bundle args, Dialog dialog){
 		
+	}
 
 		
 	
@@ -104,7 +141,7 @@ public class CreateLabel extends Activity {
 		/*
 		 * Note: Disabled EditText fields may still get focus by some other means, and allow text input.
 		 *       See http://code.google.com/p/android/issues/detail?id=2771
-		 */
+		 *
 		editText.setEnabled(set);
 		editText.setFocusable(set);
 		editText.setFocusableInTouchMode(set);
@@ -112,5 +149,5 @@ public class CreateLabel extends Activity {
     private String getSerialFieldText() {
         return serial.getText().toString();
     }
-	
+	*/
 }
